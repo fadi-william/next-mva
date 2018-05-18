93607a4a6d05f58d3a9a87894950d181631b6545
Fadi William <fadi.william.ghali@gmail.com>
2018-05-18 08:45:30 +0200

    fix(next_document.js): add the link tag during production only

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,18 +1,21 @@
-import Document, { Head, Main, NextScript } from 'next/document'
- 
+import Document, { Head, Main, NextScript } from "next/document";
+
+const isProduction = process.env.NODE_ENV === "production";
+
 export default class MyDocument extends Document {
   render() {
-   
     return (
       <html>
         <Head>
-          <link rel="stylesheet" href="/_next/static/style.css" />
+          {isProduction && (
+            <link rel="stylesheet" href="/_next/static/style.css" />
+          )}
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
