9e89301d27761928a0ff03dea635da87985958a4
Fadi William <fadi.william.ghali@gmail.com>
2018-05-18 08:39:07 +0200

    fix(show_model): check if the show's image is available
    * Check if the show's image is available to prevent console error messages.

--- app/models/show/Show.ts
+++ app/models/show/Show.ts
@@ -10,57 +10,61 @@ let store: TShow = null;
 // If the id changed, we will re-initialize the store *(on the client-side).
 let currentShowId: number = null;
 
-export const Show = types.model({
+export const Show = types
+  .model({
     id: types.maybe(types.identifier(types.number)),
     image: types.maybe(types.string),
     isLoading: types.optional(types.maybe(types.boolean), true),
     name: types.maybe(types.string),
-    summary: types.maybe(types.string),
-})
-.actions( (self) => ({
+    summary: types.maybe(types.string)
+  })
+  .actions(self => ({
     doneLoading() {
-        self.isLoading = false;
-    },
-}))
-.actions( (self) => ({
-    getShow: flow(function *() {
-        const response = yield getShow(self.id);
+      self.isLoading = false;
+    }
+  }))
+  .actions(self => ({
+    getShow: flow(function*() {
+      const response = yield getShow(self.id);
 
-        if (response.data) {
-            const show = Object.assign({}, response.data);
+      if (response.data) {
+        const show = Object.assign({}, response.data);
 
-            show.id = Number(response.data.id);
-            show.image = response.data.image.medium;
+        show.id = Number(response.data.id);
 
-            applySnapshot(self, show);
-
-            self.doneLoading();
+        if (response.data.image !== null) {
+          show.image = response.data.image.medium;
         }
-    }),
-}))
-.actions( (self) => ({
+
+        applySnapshot(self, show);
+
+        self.doneLoading();
+      }
+    })
+  }))
+  .actions(self => ({
     afterCreate() {
-        self.getShow();
-    },
-}));
+      self.getShow();
+    }
+  }));
 
 // Utility function - Instantiates a part of the mobx-state-tree store.
 export function getStore(isServer, showId, snapshot = null) {
-    // If this is the server.
-    if (isServer) {
-      currentShowId = showId;
-      store = Show.create({
-          id: showId,
-      });
-    }
-    if (store === null || currentShowId !== showId) {
-        currentShowId = showId;
-        store = Show.create({
-            id: showId,
-        });
-    }
-    if (snapshot) {
-      applySnapshot(store, snapshot);
-    }
-    return store;
+  // If this is the server.
+  if (isServer) {
+    currentShowId = showId;
+    store = Show.create({
+      id: showId
+    });
+  }
+  if (store === null || currentShowId !== showId) {
+    currentShowId = showId;
+    store = Show.create({
+      id: showId
+    });
+  }
+  if (snapshot) {
+    applySnapshot(store, snapshot);
+  }
+  return store;
 }
